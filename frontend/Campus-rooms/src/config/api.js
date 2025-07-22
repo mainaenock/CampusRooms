29a7e5276c43ca55191f9871210c9fe29c0e3218
mainaenock <mainaenock10@gmal.com>
2025-07-22 10:43:12 +0300

    logging render url

--- frontend/Campus-rooms/src/config/api.js
+++ frontend/Campus-rooms/src/config/api.js
@@ -1,5 +1,6 @@
 // API Configuration for CampusRooms
-const API_BASE_URL = 'http://localhost:3000' || import.meta.env.VITE_API_URL
+console.log('API_BASE_URL:', import.meta.env.VITE_API_URL);
+const API_BASE_URL = import.meta.env.VITE_API_URL //|| 'http://localhost:3000'
 
 export const API_ENDPOINTS = {
   // Auth endpoints
